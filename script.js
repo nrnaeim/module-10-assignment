26f049c7e9037e5971f6556ed191f36267892fc1
Niemur Rahman <nrnaeim@gmail.com>
2025-06-02 00:04:04 +0600

    Question 1 and 2 function added

---
+++ script.js
@@ -0,0 +1,35 @@
+//Question 1 answer
+document
+  .querySelector("#questionOneInput")
+  .addEventListener("input", (event) => {
+    document.querySelector("#questionOneAnswer").value = countVowels(event);
+  });
+//vowel count return function
+function countVowels(event) {
+  const str = event.target.value;
+  const vowelsArray = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (vowelsArray.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+//Question 2 Answer
+function sortNumbers(input) {
+  return input
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
+}
+
+document
+  .querySelector("#questionTwoInput")
+  .addEventListener("input", (event) => {
+    const inputvalues = event.target.value;
+    document.querySelector("#questionTwoAnswer").value =
+      sortNumbers(inputvalues).join(" ");
+  });
